df4f6912fa3c823843b138a1c70c7b580eeaadbd
Mukund-Vaghasiya13 <mukundvaghasiya138@gmail.com>
2025-01-04 17:02:00 +0530

    changes

--- app/src/main/java/com/example/recipehub/EditProfileScreen.kt
+++ app/src/main/java/com/example/recipehub/EditProfileScreen.kt
@@ -1,20 +1,20 @@
 package com.example.recipehub
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
 
 class EditProfileScreen : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_edit_profile_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val editprofilebar = findViewById<MaterialToolbar>(R.id.editprofilebar)
+        setSupportActionBar(editprofilebar)
     }
 }
